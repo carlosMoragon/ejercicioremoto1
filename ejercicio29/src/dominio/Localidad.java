bff8a3fbc6eacafa4774cdce2277f11915436c23
Carlos Moragon <cmoragoncorella@gmail.com>
2021-10-08 12:28:57 +0200

    Clase 8 de octubre

--- ejercicio29/src/dominio/Localidad.java
+++ ejercicio29/src/dominio/Localidad.java
@@ -1,14 +1,26 @@
 package dominio;
+/**
+ *Esta clase define una Localidad con su nombre y sus habitantes
+ *@author Carlos Moragón
+ */
+
 public class Localidad{
 	private String nombre;
 	private int numeroDeHabitantes;
-
+        /**
+	 *Para obtener el nombre de la localidad
+	 *@author Carlos Moragón
+	 */
 	public String getNombre(){
 		return nombre;
 	}
 	public void setNombre(String nombre){
 		this.nombre = nombre;
 	}
+	/**
+	 * Para obtener el numero de habitantes 
+	 * @author Carlos Moragón
+	 */
 	public int getNumeroDeHabitantes(){
 		return numeroDeHabitantes;
 	}
--- ejercicio29/src/dominio/Persona.java
+++ ejercicio29/src/dominio/Persona.java
@@ -1,22 +1,31 @@
 package dominio;
 import dominio.Localidad;
 public class Persona{
-String nombre;
-Localidad lugarDeNacimiento;
-public String getNombre(){
-return nombre;
-}
-public void setNombre(String nombre){
-this.nombre = nombre;
-}
-public Localidad getLugarDeNacimiento(){
-return lugarDeNacimiento;
-}
-public void setLugarDeNacimiento(Localidad
-localidad){
-lugarDeNacimiento = localidad;
-}
-public String toString(){
-return nombre + ", " + lugarDeNacimiento;
-}
+	/** 
+	 *Declaramos la variables que vamos a usar en esta clase
+	 *@author Carlos Moragón
+	 */
+	String nombre;
+	Localidad lugarDeNacimiento;
+
+	/**
+	 * Dado que las variables estan en privado debemos hacer getter y setters
+	 * @author Carlos Moragón
+	 */
+	public String getNombre(){
+		return nombre;
+	}
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public Localidad getLugarDeNacimiento(){
+		return lugarDeNacimiento;
+	}
+	public void setLugarDeNacimiento(Localidad
+			localidad){
+		lugarDeNacimiento = localidad;
+			}
+	public String toString(){
+		return nombre + ", " + lugarDeNacimiento;
+	}
 }
