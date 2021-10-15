2d852c86d2fc48ceb0edec4c601b35753f5eff30
Carlos Moragon <cmoragoncorella@gmail.com>
2021-10-15 12:27:24 +0200

    creado el ejercicio 42

---
+++ ejercicio42/src/aplicacion/Principal.java
@@ -0,0 +1,13 @@
+package aplicacion;
+public class Principal{
+	public static void main(String[] args){
+		/*int i = 0;
+		while (i <= 10){
+			System.out.println(i);
+			i += 2;
+		}*/
+		for (int i = 0; i <= 10; i += 2){
+			System.out.println(i);
+		}
+	}
+}
