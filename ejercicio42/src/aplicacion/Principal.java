519605cec49390fd8474c9f2878a2879f881fac8
Carlos Moragon <cmoragoncorella@gmail.com>
2021-10-18 12:25:52 +0200

    clase del 18 de octubre

--- ejercicio42/src/aplicacion/Principal.java
+++ ejercicio42/src/aplicacion/Principal.java
@@ -1,13 +1,54 @@
-package aplicacion;
-public class Principal{
-	public static void main(String[] args){
-		/*int i = 0;
-		while (i <= 10){
-			System.out.println(i);
-			i += 2;
-		}*/
-		for (int i = 0; i <= 10; i += 2){
-			System.out.println(i);
-		}
+package aplicacion; 
+public class Principal{ 
+	public static void main(String[]args){
+	/*int i = 0;
+	  while (i <= 10){ System.out.println(i); i += 2; }*/
+	/*for (int i = 0; i <= 10; i += 2){
+	  System.out.println(i); }*/
+	/*
+	   int i = 1; do System.out.println(i++); while (i <= 10);
+	//primero se ejecuta poniendo el valor de i y en el siguiente
+	//termino ya comprueba el i<=10
+	*/
+	/*
+	   int i = 0; int j = 0; while(i <= 10){ j = 0; while(j <= i + 5){
+	   system.out.println(i + "," + j); j++; } i++;
+	   */
+		/*
+		   int i = 1;
+		   int suma = 0;
+		   while(i <= 100){
+			   suma = suma + i;
+			   i++;
+		   }
+		   System.out.println(suma);
+	   */
+		/*
+	int suma = 0;
+	for (int i = 1; i <= 100; i++){
+		suma = suma + i;
+
 	}
+	
+	System.out.println(suma);
+	   */
+
+		int[] array = new int[10];
+
+		/*System.out.println(array[1]);
+		String[] texto = new String[5];
+		texto[0] = "Hola mundo";
+		System.out.println(texto[0]);
+		for(int i = 0; i < 10; i++){
+			System.out.println(array[i]);
+		}*/
+		int i = 0;
+		while(i < array.length){
+			System.out.println(array[i]);
+			i++;
+		//array.length te da el numero de posiciones que tiene mi array
+		}
+
+	   }
+
 }
