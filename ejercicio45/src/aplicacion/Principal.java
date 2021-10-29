d55a88416b60df4c278695822ac2efe4f62eacd3
Carlos Moragon <cmoragoncorella@gmail.com>
2021-10-29 12:36:26 +0200

    Terminado ejercicio 47

--- ejercicio45/src/aplicacion/Principal.java
+++ ejercicio45/src/aplicacion/Principal.java
@@ -4,46 +4,61 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.File;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Principal{
 	public static void main(String[] args){
-	//Sytem.in para pedir texto a la consola
-	/*Scanner sc = new Scanner(System.in);
-	String texto = "";
-	while(sc.hasNext()){
-		texto += sc.next() + " ";
-	}
-	System.out.println(texto);
+		//Sytem.in para pedir texto a la consola
+		/*Scanner sc = new Scanner(System.in);
+		  String texto = "";
+		  while(sc.hasNext()){
+		  texto += sc.next() + " ";
+		  }
+		  System.out.println(texto);
 
-	}
-	System.out.println("Introduzca su nombre: ");
-	String nombre = sc.next();
-	System.out.println("Su nombre es " + nombre);
-	sc.close();*/
-	/*//Opcion 1
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Dime un entero:");
-	int numero1 = sc.nextInt();
-	System.out.println("Dime otro entero:");
-	int numero2 = sc.nextInt();
-	int resultado = numero1 + numero2;
-	System.out.println("Esta es su suma: " + resultado);
-	sc.close();
-	//Opcion 2
-	int suma = 0;
-	for(int i = 0; i < 2; i++){
+
+		  System.out.println("Introduzca su nombre: ");
+		  String nombre = sc.next();
+		  System.out.println("Su nombre es " + nombre);
+		  sc.close();*/
+		/*//Opcion 1
+		  Scanner sc = new Scanner(System.in);
+		  System.out.println("Dime un entero:");
+		  int numero1 = sc.nextInt();
+		  System.out.println("Dime otro entero:");
+		  int numero2 = sc.nextInt();
+		  int resultado = numero1 + numero2;
+		  System.out.println("Esta es su suma: " + resultado);
+		  sc.close();
+		//Opcion 2
+		int suma = 0;
+		for(int i = 0; i < 2; i++){
 		System.out.println("Introduzca un numero entero:");
 		suma += sc.nextInt();
+		}
+		System.out.println("El resultado es: " + suma);
+		sc.close();
+		*/
+
+
+		Scanner sc = new Scanner(System.in);
+
+		boolean valor = true;
+		try{
+			while(valor){
+				int suma = 0;
+				for(int i = 0; i < 2 ; i++){
+					System.out.println("Dime un numero");
+					suma += sc.nextInt();
+				}
+				System.out.println("El resultado es:" + suma);
+				valor = false;
+				sc.close();
+			}catch(InputMismatchException e){
+				System.out.println("prueba con un numero");
+
+			}
+			System.out.println("El resultado es:" + suma1);
+			sc1.close();
+		}
 	}
-	System.out.println("El resultado es: " + suma);
-	sc.close();
-	*/
-	
-	Scanner sc = new Scanner(System.in);
-	int suma = 0;
-	for(int i = 0; i 
-	System.out.println("Dime un numero");
-
-
-}
-
 }
--- ejercicio47/src/aplicacion/Principal.java
+++ ejercicio47/src/aplicacion/Principal.java
@@ -1,12 +1,13 @@
 package aplicacion;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Principal{
 	public static void main(String[] args){
-		System.out.println("Introduzca un numero: ");
+		/*System.out.println("Introduzca un numero del 1 al 7: ");
 		Scanner sc = new Scanner(System.in);
 		int numero = sc.nextInt();
-		sc.close();
-		switch(numero){
+		sc.close();*/
+		/*switch(numero){
 			//case 1 es para cuando la variable vale 1
 			//break es para que se pare de ejecutar el codico (para delimitar el codigo)
 			case 1:
@@ -18,7 +19,41 @@ public class Principal{
 			//dafault es para que se ejecute si no se han cumplido los casos previos
 			default:
 				System.out.println("El numero no es ni 1 ni 2");
-		}
-	}
+		}*/
+		boolean valor = true;
+	while(valor){
+		try{
+		System.out.println("Intruzca un numero del 1 al 7: ");
+		Scanner sc = new Scanner(System.in);
+		int numero = sc.nextInt();
+		valor = false;
+		switch(numero){
+			case 1: System.out.println("Lunes");
+				break;
+			case 2: System.out.println("Martes");
+				break;
+			case 3: System.out.println("Miercoles");
+				break;
+			case 4: System.out.println("Jueves");
+				break;
+			case 5: System.out.println("Viernes");
+				break;
+			case 6: System.out.println("Sabado");
+				break;
+			case 7: System.out.println("Domingo");
+				break;
+			default: System.out.println("Eso no es un numero del 1 al 7");
+				 valor = true;
 
+		}
+		sc.close();
+		}catch(InputMismatchException e){ 
+			
+			System.out.println("Intentalo de nuevo");
+			
+			 
+		}
+	}  
+
+}
 }
