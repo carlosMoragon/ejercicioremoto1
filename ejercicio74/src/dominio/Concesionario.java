40c8b8cfb6eac5236728c658da37c6a62e8f7f0a
Carlos Moragon <cmoragoncorella@gmail.com>
2021-12-08 02:34:04 +0100

    editado ejercicio 74

--- ejercicio74/src/dominio/Concesionario.java
+++ ejercicio74/src/dominio/Concesionario.java
@@ -1,12 +1,12 @@
 package dominio;
 
 public class Concesionario{
-
+	/*
 	VehiculoTurismo audi = new VehiculoTurismo("audi", "a1", 21.900);
 	VehiculoTurismo seat = new VehiculoTurismo("seat", "ibiza", 16.520);
 	VehiculoTurismo suzuki = new VehiculoTurismo("suzuki", "across", 53.700);
 	
 	audi.precioFinal(5);
 	seat.precioFinal(4);
-	suzuki.precioFinal(7);
+	suzuki.precioFinal(7);*/
 }
