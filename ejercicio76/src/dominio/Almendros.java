2adadee001026a08eec06cbde5de34a76bd810a6
Carlos Moragon <cmoragoncorella@gmail.com>
2021-12-27 21:07:52 +0100

    avanzando en el ejercicio 76

--- ejercicio76/src/dominio/Almendros.java
+++ ejercicio76/src/dominio/Almendros.java
@@ -4,8 +4,8 @@ public class Almendros extends Parcela{
 	private float kilosAlmendra = 0;
 	private float escandallo = 0;
 	private int precioAlmendraLimpia = 0;
- 	public Almendros(String nombre, int piesDeArbol, double ingresosTotales, double gastosTotales, double beneficios,float kilosAlmendra, float escandallo, int precioAlmendraLimpia){
-		super(nombre, piesDeArbol, ingresosTotales, gastosTotales, beneficios);
+ 	public Almendros(String nombre, int piesDeArbol, float ingresosTotales, float gastosTotales, float kilosAlmendra, float escandallo, int precioAlmendraLimpia){
+		super(nombre, piesDeArbol, ingresosTotales, gastosTotales);
 		this.kilosAlmendra = kilosAlmendra;
 		this.escandallo = escandallo;
 		this.precioAlmendraLimpia = precioAlmendraLimpia;
